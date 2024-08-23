7f292a048e29e822af337b83839516acecb4358f
joaBotto <joaquinbotto2@hotmail.com>
2024-08-22 22:24:12 -0300

    Primeros pasos

---
+++ funciones.js
@@ -0,0 +1,9 @@
+function encriptar(texto) {
+  let textoEncriptado = texto
+    .replace(/e/g, "enter")
+    .replace(/i/g, "imes")
+    .replace(/a/g, "ai")
+    .replace(/o/g, "ober")
+    .replace(/u/g, "ufat");
+  return textoEncriptado;
+}
